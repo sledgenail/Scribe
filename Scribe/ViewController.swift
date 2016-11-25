7f73dcb9b3e21d53a7ad380939d73fef385064ea
Emmanuel Erilibe <sledgenail@gmail.com>
2016-11-25 08:52:09 -0600

    Initial Commit

---
+++ Scribe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scribe
+//
+//  Created by Emmanuel Erilibe on 11/25/16.
+//  Copyright © 2016 Emmanuel Erilibe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
